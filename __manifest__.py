b2f2366b4d3e59d37f77ca9e474c981c90f02d63
Jhonnel-Isep <80534394+Jhonnel-Isep@users.noreply.github.com>
2021-04-07 15:25:26 -0400

    Todo listo a debuggear

--- __manifest__.py
+++ __manifest__.py
@@ -2,6 +2,6 @@
     'name': 'Asigna leads', 
     'description': 'Asigna leads automaticamente a agentes de ventas en funcion de disponibilidad y tasa de conversión', 
     'author': 'Jhonnel Nuñez, Farjan Rondón', 
-    'depends': ['base','crm'], 
+    'depends': ['base', 'crm', 'security_user_roles'], 
     'application': True, 
 }
