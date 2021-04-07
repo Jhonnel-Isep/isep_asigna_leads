2897819984ff03a2d36572ff4d10e2dce1f8d470
Jhonnel-Isep <80534394+Jhonnel-Isep@users.noreply.github.com>
2021-04-07 15:26:14 -0400

    todo listo a debuggear

--- models/grupo_agentes.py
+++ models/grupo_agentes.py
@@ -3,15 +3,10 @@ from odoo import models, fields
 # 
 # 
 #
-# Queda pentiene:
-# -Crear los grupo_agentes en la bdd y asignar los agentes a estos grupos 
-#  (Los agentes estan en el excel)
-# -La funcion que asigna el lead
-# 
 #
 # Estructura:
-# -Clase Acciones al crear lead
-# -Clases con datos de dias festivos (Para calcular la asignacion del lead)
+# 
+# 
 #--------------------------------------------------------------------------
 
 
@@ -24,135 +19,125 @@ class CrmLead(models.Model):
         self.localidad = values.get('country_id')
         # 68 = España, 
 
-        if agente:
-            agente = self.asigna_agente()
-        
+        if not agente:
+            asigna_agente()
+            values.update({'user_id': selec_agente()})
 
-    def asigna_agente(self, values): # Aqui va la funcion que asigna el agente al lead
-        
-        dic_agents = {}
+    def asigna_agente(self):
+        list_agentes = []
+        list_cant_leads = []
+        list_tasat_conv = []
 
-        if self.localidad == 68: # Se asigna a agentes de España
-          li_agents = self.env['security.rol'].search([('')]) # Farjan Roles de agentes España
-            for agentes in li_agents:
+        if (self.localidad == 68):
+
+          li_agentes = self.env['security.role'].browse().user_ids # id = ?
+
+          for agente in li_agentes:
+
+            list_agentes.append(agente)
+
+            cant_leads = 0
+            tasa_conv = []
+
+            leads_agent = self.env['crm.lead'].search([('user_id', '=', agente.id)])
+
+            for leads in leads_agent:
+
+              if (leads.type == 'lead'):
+                cant_leads += 1
+
+              tasa_conv.append(leads.probability)
+
+            list_cant_leads.append(cant_leads)
+            tasat_conv = sum(tasa_conv) / len(tasa_conv)
+            list_tasat_conv.append(tasat_conv)
+
+          self.dic_agents = {'Agente':list_agentes, 'Numero de Leads':list_cant_leads, 'Tasa de Conversion':list_tasat_conv}
+
+
+
+        else:
+
+          li_agentes = self.env['security.role'].browse().user_ids # id = ?
+
+          for agente in li_agentes:
+
+            if not(agente.country_id.name.lower() in filtro_feriado()):
+              # aqui se filtran agentes dia feriado
+              list_agentes.append(agente)
+
               cant_leads = 0
               tasa_conv = []
 
-              leads_agent = self.env['crm.lead'].search([('user_id', '=', agente.id)]) # Farjan deberia guardar los leads del agente de españa
-              
+              leads_agent = self.env['crm.lead'].search([('user_id', '=', agente.id)])
+
               for leads in leads_agent:
-                if leads.type == "lead": # Farjan si el crm_lead es un "lead" lo suma
-                  cant_leads +=1
-              for leads in leads_agent:
-                tasa_conv.append(leads.probability) # Farjan crea una lista con las tasas de conversion
-              
-              tasat_conv = sum(tasa_conv)/len(tasa_conv) # calcula la tasa de conversion
 
-              dic_agents.append('Agente':agentes, 'Numero de leads':cant_leads, 'tasa conversion':tasat_conv) # Retorna al agente con el numero de leads y tasa de conversion
-            
+                if (leads.type == 'lead'):
+                  cant_leads += 1
+
+                tasa_conv.append(leads.probability)
+
+              list_cant_leads.append(cant_leads)
+
+              tasat_conv = sum(tasa_conv) / len(tasa_conv)
+              list_tasat_conv.append(tasat_conv)
+
+          self.dic_agents = {'Agente':list_agentes, 'Numero de Leads':list_cant_leads, 'Tasa de Conversion':list_tasat_conv}
 
 
-        else: # Se asigna a agente de latam  
-          li_agents = self.env['security.rol'].search([('')]) # Farjan Roles de agentes LATAM
-            for agentes in li_agents:
-              cant_leads = 0
-              tasa_conv = []
-              leads_agent = self.env['crm.lead'].search([('user_id', '=', agente.id)]) # Farjan deberia guardar los leads del agente de LATAM
-              for leads in leads_agent:
-                if leads.type == "lead": # Farjan si el crm_lead es un "lead" lo suma
-                  cant_leads +=1
-              for leads in leads_agent:
-                tasa_conv.append(leads.probability) # Farjan crea una lista con las tasas de conversion
-              tasat_conv = sum(tasa_conv)/len(tasa_conv) # calcula la tasa de conversion
+    def filtro_feriado(self):
+      
+      import datetime
 
-              dic_agents.append('Agente':agentes, 'Numero de leads':cant_leads, 'tasa conversion':tasat_conv) # Retorna al agente con el numero de leads y tasa de conversion
-        
+      now = datetime.datetime.now()
+      dia = int(now.strftime("%d"))
+      mes = int(now.strftime("%m"))
 
-        return agente
+      fecha = (mes, dia)
 
+      paises=[]
+      #mes, dia
+      mexico = [(1,1),(2,1),(3,15),(4,4),(5,1),(9,16),(11,15),(12,25)]
+      colombia = [(1,1),(1,11),(3,22),(5,1),(5,17),(6,7),(6,14),(7,5),(7,20),(8,7),(8,16),(10,18),(11,1),(11,15),(12,8),(12,25)]
+      salvador = [(1,1),(5,1),(5,10),(6,17),(8,5),(8,6),(9,15),(11,2),(12,25)]
+      nicaragua = [(1,1),(5,1),(7,19),(8,1),(8,10),(9,14),(9,15),(12,8),(12,25)]
+
+      for i in mexico:
+          if fecha==i:
+              paises.append("mexico")
+      for i in colombia:
+          if fecha==i:
+              paises.append("colombia")
+      for i in salvador:
+          if fecha==i:
+              paises.append("salvador")
+      for i in nicaragua:
+          if fecha==i:
+              paises.append("nicaragua")
+
+      return paises
+    
     def selec_agente(self):
+      menor=10000
+      lista=[]
+      index=0
+      mayor=0
+      for i in self.dic_agents["Numero de Leads"]:
+           if i < menor:
+               menor=i
+      for i in self.dic_agents["Numero de Leads"]:
+          if i ==menor:
+              lista.append(self.dic_agents["Agente"][index])
+          index+=1
+
+      index=0
+      for i in lista:
+         if i > mayor:
+             mayor =i
+             index+=1
+      agente = lista[index-1]
+
+      return agente # retorna el agente seleccionado
 
 #-----------------------------------------------------------
-
-# -Clases con datos de dias festivos (Para calcular la asignacion del lead)
-# España
-class EsBusinessCalendar(AbstractHolidayCalendar):
-   rules = [
-     Holiday('Año Nuevo', month=1, day=1),
-     Holiday('Epifanía del Señor', month=1, day=6),
-     Holiday('Viernes Santo', month=4, day=2),
-     Holiday('Día del Trabajador', month=5, day=1),
-     Holiday('Asunción de la Virgen', month=8, day=15),
-     Holiday('Día de la Hispanidad', month=10, day=12),
-     Holiday('Todos los Santos', month=11, day=1),
-     Holiday('Día Constitución', month=12, day=6),
-     Holiday('Inmaculada Concepción', month=12, day=8),
-     Holiday('Navidad', month=12, day=25)
-   ]
-# Mexico
-class EsBusinessCalendar2(AbstractHolidayCalendar):
-   rules = [
-     Holiday('Año Nuevo', month=1, day=1),
-     Holiday('Día de la Constitución Mexicana', month=2, day=1),
-     Holiday('Natalicio de Benito Juárez', month=3, day=15),
-     Holiday('Domingo de Resurreción', month=4, day=4),
-     Holiday('Día del Trabajador', month=5, day=1),
-     Holiday('Día de la Independencia', month=9, day=16),
-     Holiday('Revolución Mexicana', month=11, day=15),
-     Holiday('Navidad', month=12, day=25)
-   ]
-# Colombia
-class EsBusinessCalendar3(AbstractHolidayCalendar):
-   rules = [
-     Holiday('Año Nuevo', month=1, day=1),
-     Holiday('Día de los Reyes Magos', month=1, day=11),
-     Holiday('Día de San José', month=3, day=22),
-     Holiday('Jueves Santo', month=4, day=1),
-     Holiday('Viernes Santo', month=4, day=2),
-     Holiday('Día del Trabajador', month=5, day=1),
-     Holiday('Día de la Ascensión', month=5, day=17),
-     Holiday('Corpus Cristi', month=6, day=7),
-     Holiday('Sagrado Corazón', month=6, day=14),
-     Holiday('San Pedro y San Pablo', month=7, day=5),
-     Holiday('Día de la Independencia', month=7, day=20),
-     Holiday('Batalla de Boyacá', month=8, day=7),
-     Holiday('La asunción de la Virgen', month=8, day=16),
-     Holiday('Día de la Raza', month=10, day=18),
-     Holiday('Día de los Difuntos', month=11, day=1),
-     Holiday('Independencia de Cartagena', month=11, day=15),
-     Holiday('Día de la Inmaculada Concepción', month=12, day=8),
-     Holiday('Navidad', month=12, day=25)
-   ]
-# El Salvador
-class EsBusinessCalendar4(AbstractHolidayCalendar):
-   rules = [
-     Holiday('Año Nuevo', month=1, day=1),
-     Holiday('Jueves Santo', month=4, day=1),
-     Holiday('Viernes Santo', month=4, day=2),
-     Holiday('Sábado Santo', month=4, day=3),
-     Holiday('Domingo Santo', month=4, day=4),
-     Holiday('Día del Trabajador', month=5, day=1),
-     Holiday('Día de la Madre', month=5, day=10),
-     Holiday('Día del Padre', month=6, day=17),
-     Holiday('Fiestas de San Salvador', month=8, day=5),
-     Holiday('Celebración del Divino Salvador del Mundo', month=8, day=6),
-     Holiday('Día de la Independencia', month=9, day=15),
-     Holiday('Día de los Difuntos', month=11, day=2),
-     Holiday('Navidad', month=12, day=25)
-   ]
-# Nicaragua
-class EsBusinessCalendar5(AbstractHolidayCalendar):
-   rules = [
-     Holiday('Año Nuevo', month=1, day=1),
-     Holiday('Jueves Santo', month=4, day=1),
-     Holiday('Viernes Santo', month=4, day=2),
-     Holiday('Domingo Santo', month=4, day=4),
-     Holiday('Día del Trabajador', month=5, day=1),
-     Holiday('Día de la Revolución', month=7, day=19),
-     Holiday('Traída de Santo Domingo de Guzmán', month=8, day=1),
-     Holiday('Dejada de Santo Domingo de Guzmán', month=8, day=10),
-     Holiday('Batalla de San Jacinto', month=9, day=14),
-     Holiday('Independencia de Nicaragua', month=9, day=15),
-     Holiday('Inmaculada Concepción de María', month=12, day=8),
-     Holiday('Navidad', month=12, day=25)
-   ]
