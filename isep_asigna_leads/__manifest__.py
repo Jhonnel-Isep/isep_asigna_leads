6cb520c95407fdcad9ae91999569d6e8dd70a61d
Jhonnel-Isep <80534394+Jhonnel-Isep@users.noreply.github.com>
2021-04-08 08:30:12 -0400

    Delete isep_asigna_leads directory

--- isep_asigna_leads/__manifest__.py
+++
@@ -1,7 +0,0 @@
-{ 
-    'name': 'Asigna leads', 
-    'description': 'Asigna leads automaticamente a agentes de ventas en funcion de disponibilidad y tasa de conversión', 
-    'author': 'Jhonnel Nuñez, Farjan Rondón', 
-    'depends': ['base', 'crm', 'security_user_roles', 'crm_lead', 'res_users'], 
-    'application': True, 
-}
