d5bb6b1ad34fa030ff4d30f9932ddd01606275cf
farondon <80845366+farondon@users.noreply.github.com>
2021-04-20 09:55:24 -0400

    Add files via upload

--- models/CODE.py
+++ models/CODE.py
@@ -4,11 +4,12 @@ class CrmLead(models.Model):
 	_inherit = 'crm.lead'
 
 	@api.model
-	def create(self, values): # Cuando se crea el lead se ejecuta para asignar agente
+	def create(self, values):
 		self.agente = values.get('user_id')
 		self.telf = values.get('phone')
 		self.mail = values.get('email_from')
 		self.localidad = values.get('country_id')
+		self.fecha_entrada = values.get('create_date')
 		# 68 = España, 
 
 		if not self.agente:
@@ -25,7 +26,7 @@ class CrmLead(models.Model):
 
 		if (self.localidad == 68):
 
-			li_agentes = self.env['security.role'].browse(139).user_ids
+			li_agentes = self.env['security.role'].browse(2).user_ids ### --> COLOCAR EL ID DEL ROL DE ESPAÑA <-- ###
 
 			for agente in li_agentes:
             
@@ -59,7 +60,7 @@ class CrmLead(models.Model):
 
 		else:
 
-			li_agentes = self.env['security.role'].browse(140).user_ids
+			li_agentes = self.env['security.role'].browse(3).user_ids ### --> COLOCAR EL ID DEL ROL DE LATAM <-- ###
 
 			for agente in li_agentes:
 
@@ -117,12 +118,79 @@ class CrmLead(models.Model):
 				paises.append("colombia")
 		for i in salvador:
 			if fecha==i:
-				paises.append("salvador")
+				paises.append("el salvador")
 		for i in nicaragua:
 			if fecha==i:
 				paises.append("nicaragua")
 
 		return paises
+
+
+	def filtro_horario(self):
+
+		import datetime
+
+		week = int(self.fecha_entrada.weekday())
+		hora = int(self.fecha_entrada.strftime('%H'))
+		minu = int(self.fecha_entrada.strftime('%M'))
+
+		paises = []
+
+		if (hora-4 in range(9,17)) and (week in range(4)):
+			paises.append('venezuela')
+
+
+		if (hora-5 in range(9,17)) and (week in range(5)):
+			if (week == 5):
+				if (hora-5 < 12):
+					paises.append('colombia')
+					paises.append('mexico (cdmx)')
+				else:
+					paises.append('mexico (cdmx)')
+			else:
+				paises.append('colombia')
+				paises.append('mexico (cdmx)')
+
+
+		if (hora-6 in range(9,17)) and (week in range(5)):
+			if (week == 5):
+				paises.append('mexico (la paz)')
+			else:
+				paises.append('nicaragua')
+				paises.append('el salvador')
+				paises.append('mexico (la paz)')
+
+
+		if (hora-7 in range(9,17)) and (week in range(5)):
+			paises.append('tijuana')
+
+
+		if (len(paises) == 0):
+			self.viejo_lead()
+
+		else:
+			return paises
+
+
+	
+	def preferencia_pais(self):
+
+		lead_pais = self.localidad
+		agen_pais = self.env['res.users'].search([('security_role_ids', '=', 'Ventas / Asesor'), ('country_id', '=', lead_pais)])
+
+		agentes_mismo_pais = []
+
+		for agentes in agen_pais:
+			agentes_mismo_pais.append(agentes.id)
+
+		if (len(agentes_mismo_pais) == 0):
+			##########
+			# Llamar a la siguiente condición (numero maximo de leads) y retornarle la lista de agentes anterior a este filtro
+			##########
+
+		else:
+			return agentes_mismo_pais
+
 
 	
 	def viejo_lead(self):
@@ -138,7 +206,7 @@ class CrmLead(models.Model):
 		for lead in todos_leads:
 			telefonos.append(lead.phone)
 			emails.append(lead.email_from)
-			agentes.append(lead.x_contactonuevoodup12)
+			agentes.append(lead.user_id) ### --> COLOCAR EL FIELD DE "ACTUAL" <-- ###
 
 		li_todos_leads = [telefonos, emails, agentes]
 			
@@ -174,7 +242,7 @@ class CrmLead(models.Model):
 			agente = self.selec_agente()
 
 			##########
-			# Este else es para colocar la funcion que se debe ejecutar en el caso de que el lead no haya sido atendido antes.
+			# Este 'else' es para colocar la funcion que se debe ejecutar en el caso de que el lead no haya sido atendido antes.
 			##########
 
 		return agente
