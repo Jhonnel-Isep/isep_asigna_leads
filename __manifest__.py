89b37b678a7eb93ae2f9ee93089f07a5c5a7f69c
farondon <80845366+farondon@users.noreply.github.com>
2021-04-30 12:09:42 -0400

    Add files via upload

--- __manifest__.py
+++ __manifest__.py
@@ -2,6 +2,19 @@
     'name': 'Asigna leads', 
     'description': 'Asigna leads automaticamente a agentes de ventas en funcion de disponibilidad y tasa de conversión', 
     'author': 'Jhonnel Nuñez, Farjan Rondón', 
-    'depends': ['base', 'crm', 'security_user_roles'],
+    
+    'depends': [
+        'base', 
+        'crm', 
+        'security_user_roles'
+    ],
+    
+    'data': [
+        'views/AtributosAgentes.xml',  
+        'views/AutoLeadLogRegister.xml',
+
+        'menu/isep_asigna_leads_menu.xml'
+    ],
+    
     'application': True, 
 }
